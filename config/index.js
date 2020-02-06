75734fca45bcf487d5e90165b2b1644db5ecbbb8
elva329 <jia_shen@126.com>
2020-02-06 17:50:12 +0800

    updated assets public path

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',  //update the project folder path when put to server
+    assetsPublicPath: '/travel',  //update the project folder path when put to server
 
     /**
      * Source Maps
